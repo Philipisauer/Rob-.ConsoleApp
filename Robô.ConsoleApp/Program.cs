434b06fad78a5a56238bd2c95594ffdc52d6b144
Philipi Sauer <philipisauer09@gmail.com>
2025-03-26 17:04:13 -0300

    Adicionar arquivos de projeto.

---
+++ Robô.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Robô.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
