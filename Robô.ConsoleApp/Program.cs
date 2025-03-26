7e20ab908812dd510e2e049647c158c93cc8a299
Philipi Sauer <philipisauer09@gmail.com>
2025-03-26 17:45:58 -0300

    codigos base das cordenadas e direções

--- Robô.ConsoleApp/Program.cs
+++ Robô.ConsoleApp/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+            int posicaoX1 = 1;
+            int posicaoY1 = 2;
+
+            Console.WriteLine("digite a cordenada X: ");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("digite a cordenada Y: ");
+            int Y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("digite a direçao: ");
+            double direcao = Convert.ToDouble(Console.ReadLine());
+
+
+
+            posicaoX1 += X;
+            posicaoY1 += Y;
+
+            Console.WriteLine($"X:{posicaoX1}");
+
+            Console.WriteLine($"Y:{posicaoY1}");
+
+            Console.WriteLine($"direcao:{direcao}");
+
+            if ( posicaoX1 > 50 )
+                Console.WriteLine("posiçao ultrapassa o limite!");
+
+            if (posicaoY1 > 50)
+                Console.WriteLine("posiçao ultrapassa o limite");
+
+            Console.ReadLine();
         }
     }
 }
